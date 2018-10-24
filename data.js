0e637683274fbdbac8d69dbce07b7ae8670bd797
Lauren Reilly <LaurenReillyNet@gmail.com>
2018-10-24 16:56:28 -0400

    added images for cards and completed array of all major Arcana

--- data.js
+++ data.js
@@ -4,153 +4,153 @@ var majorArcana = [
         past: "Impulsivity and rash actions have led to time wasting activities. You may have been invovled with an unconventional person who has led you astray.",
         present: "Your search to find who you truly are is causing you to abandon commitments and constraints. Your sense of free-will and openness allows you to truly sieze the day.",
         future: "You will enjoy the fresh beauty of a new life off of the beaten path. An exciting person who you view as very unique may soon captivate you.",
-        image: 
+        image: "images/fool.png" 
     },
     {
         name: "The Magician",
         past: "You've displayed your natural talents and you are enjoying success based on your accomplishments.",
         present: "You have a chance to use your unique skills to impress important individuals. Luck and skill are in your favor, sieze this opportunity!",
         future: "You've taken the time to build the skills needed to earn everything you desire. When opportunity arises you can follow the necessary path to achieve your dream.",
-        image: 
+        image: "images/magician.png"
     },
     {
         name: "The High Priestess",
         past: "You've needed to have great patience and understanding. You have an ability to get by on your gut feelings and the knowledge you've aquired is a steady foundation for the future.",
         present: "Avoid the current conflict. What is being presented to you is not likely true. You may feel that your current situation can be improved, you are correct.",
         future: "That hunch you have about a coming conflict is correct. Any attempts to get you involved in consipiring against someone should be avoided at all costs.",
-        image: 
+        image: "images/priestess.png"
     },
     {
         name: "The Empress",
         past: "Have you been acting like a diva lately? Immersed in material possesions? This is likely why you're having difficulty in current relationships.",
         present: "Things are great right now, so great that you might be feeling a little bored. Maybe its time to delve deeper into a hobby or relationship for greater satisfaction?",
         future: "Good things are coming your way, like fairytale ending kind of good. Wealth, love, and happiness are in store.",
-        image: 
+        image: "images/empress.png"
     },
     {
         name: "The Emperor",
         past: "This card represents an authority figure in your life who set a firm boundary of what is acceptable for you. If you are in a good place you have this person to thank, however if this person abused their authority they are to blame for your current feelings of loneliness and alienation.",
         present: "Be confident in following a straight and narrow path to your current goal. An outside influencer may have helped you to see this path more clearly, move forward boldly.",
         future: "Your present decisions no matter how small will have a large impact on the near future. A change will soon storm into your life demanding that you follow a specific behavior and will require that you either submit or resist.",
-        image: 
+        image: "images/emperor.png"
     },
     {
-        name: "The Heirophant",
+        name: "The Hierophant",
         past: "Your past was defined largely by a structured culture or system of belief. There is a strong sense of identity tied up in your past.",
         present: "You have begun to take interest in a new belief system whether social, political, or even a new daily routine. Your engagement and adherence offers a satisfying feeling of belonging and accomplishment.",
         future: "A person will enter your life who will explain the meaning of life to you as well as your role in it. Depending on the person, this could be a great experience or very detrimental to you. Be open but wary.",
-        image: 
+        image: "images/hierophant.png"
     },
     {
         name: "The Lovers",
         past: "The foundation of your current situation is based largely in a loving relationship. If you are in a long term union this is a confirmation that your relationship defines your identity. If you are single, this could point to a past love or even the relationship between your parents.",
         present: "You are bubbling with passion over a budding romance. If you are single now, this card signifies a sudden end to that status. The power of this relationship will be transformative over all aspects of your life.",
         future: "Approach the future of your dating life with a clear idea of what you want and need. The intense affection of new relationships can blind you to reality, so have a clear picture of your ideals before this person enters your life.",
-        image: 
+        image: "images/lovers.png"
     },
     {
         name: "The Chariot",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "A great success in the past has led you to where you are now. Sometimes early successes in life can lead to empty feelings later on, these advantages may be responsible for your current comfort or current issue.",
+        present: "You are on top of the world right now! You've achieved something you've been working at for a while. Don't be afraid if you're having mixed feelings or are feeling insecure about this recent success. Change can upheave us and prevent us from seeing the possibilities of our situation.",
+        future: "The goal you are seeking is most definitely attainable. Be full of confidence as you go forward to achieve what you have set out to do!",
+        image: "images/chariot.png"
     },
     {
         name: "Strength",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "Despite the odds you've been able to keep on top of things and you are finally benefiting from your patience and expertise. You may have strayed from a state of boredom and explored a new area of interest due to a desire to challenge yourself.",
+        present: "You are in the middle of learning how to best control your world. You are not tyrannical in this control, you are cooperative and understanding. You have created a positive environment for interaction.",
+        future: "Whatever is bothering you presently will soon be under your control though they may seem beyond your power at the moment. Get to know and appreciate yourself and your strengths as they will help you to overcome.",
+        image: "images/strength.png"
     },
     {
         name: "The Hermit",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "If you've recently completed a program of study, your success was based on your own merits and dedication to solitary study. If you haven't been in any kind of training or educational program you have likely been isolating yourself from your destiny. You've been mixed up with a bad crowd or even gotten wrapped up in a cultish mentality.",
+        present: "You are lonely. Even if you are in a relationship you feel alone and may be fooling yourself to the reality of your situation. If you are happy in a relationship consider the posibility of an abandoned person who may be seeking you.",
+        future: "If you are in a current happy relationship this is not a good card to see as it generally points to you being alone in the near future. However, if you are in an unhealthy relationship, stay strong, you will soon be free of your tormentor.",
+        image: "images/hermit.png"
     },
     {
         name: "Wheel of Fortune",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "A very large change in your life has drastically changed the course of your life and led you to where you are today. This change may have been out of your control, like the divorce of your parents, or being laid off of work.",
+        present: "Fate will smile on you and transform the circumstances of your life into a manifestation of your self awareness. If you are at a point in your life where you can recognize where you are letting yourself down, you have reached a great level of self awareness. This card reinforces the power that comes with owning your own decisions.",
+        future: "There is a big change ahead that could be very disruptive. Even if you are happy in your life now, this card signifies that nothing is stable for too long. However this card is a reminder that this too shall pass.",
+        image: "images/wheel.png"
     },
     {
         name: "Justice",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "A choice you made in the past has led you to where you are today. That choice marked the beginning of a new chapter of your life and a severance of your past and any opportunites that may have been on that path.",
+        present: "You are facing a situation that requires a decision. The card doesn't indicate the nature or size of this decision only that it is irreversible. Your free will in making this choice will be a force that creates a foundation on which the rest of your life will rest.",
+        future: "The things you are currently working on are inevitably leading you to making a huge decision. Get ready for a big change no matter how seemingly small the decision you make is.",
+        image: "images/justice.png" 
     },
     {
         name: "The Hanged Man",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "Your current situation is the result of letting go, of not being controlled by temptations. You chose to leave things behind.",
+        present: "You are being urged to avoid conflict. Someone may be desperately trying to get you to fight but the best way to defeat them is to avoid doing what they want you to do! Avoid this confrontation.",
+        future: "A struggle or large conflict is in store for you. The best way to proceed is to stay out of it. Avoid the battle and watch the enemy destroy themself.",
+        image: "images/hanged.png"
     },
     {
         name: "Death",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "You have gone through a great transformation that likely involved a loss and an inability to do anything about it. The situation no longer has power over you save in the form of bad memories, though it has certainly led you to where you are today.",
+        present: "You are paring things down in order to focus on a massive change in your life. A breakup may be in store, you may be worried about your job, or a tragic loss. No matter what, this situation is impossible to fight. As painful as it is you must accept this fate.",
+        future: "Something important in your life will soon come to an end. A close friendship, or relationship may soon dissolve. A job or investment that feels secure now may be at risk of being lost.",
+        image: "images/death.png"
     },
     {
         name: "Temperance",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "Your world has a very calm foundation. The peacefulness of your childhood ensures that your present is never too rattled. You may have recently shown great character and integrity in a situation that you could have easily shifted to your own personal advantage.",
+        present: "You are finding common ground in a dispute. Moving forward, despite past disagreements, things should be equitable. Abandon any extreme thoughts and you'll be happier for it.",
+        future: "Though you may be struggling now, and your past may seem forgettable, the future wil be full of peace and wellbeing. This card signifies an end to a current struggle and a positive future.",
+        image: "images/temperance.png"
     },
     {
         name: "The Devil",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "Your dealings with a domineering person or situation are over. Your life foundation and the way it operates may have traces of this relationship at its core but you have new aspects of life to pull from now. Those chains have been broken.",
+        present: "This is a wake-up call! You're hooked into bad habits or a bad relationship and may not even realize it. Some terrible connection in your life is holding you back from being who you truly are.",
+        future: "This is a warning. The structures you are building in your life are attracting people who will use you and take from you and won't help you accomplish your goals. If you work hard to sopt the areas in your life you are lying to yourself about you can minimize or avoid these negative impacts.",
+        image: "images/devil.png"
     },
     {
         name: "The Tower",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "The upheaval that is about to happen is actually rooted in your past. Old wounds may be reopened, something neglected may finally break, whether you are ready for these things to happen or not.",
+        present: "Any situation, love, money, jobs, can be affected. This chaos is usually contstrained to just one area of your life but the effects are very complete. There will likely be a huge loss in one area and a gain in another.",
+        future: "The coming chaos is no surprise to you. You probably already have a good idea of what is about to come crashing down. This is not a prediction of something coming months away, but inevitable in mere days or hours.",
+        image: "images/tower.png"
     },
     {
         name: "The Star",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "A great idea you had in the past has led you to your present circumstances. If you are in a good place you have your intuition to thank. If you are lost you may have followed an impractical ideal that didn't quite pan out for you.",
+        present: "You are on the edge of making some very big plans for a new direction to take your life in. You may want to contribute something grand to humanity. Idealism surrounding love is strong. Those around you may offer truths that you don't want to hear right now but they will be valuable to you soon enough.",
+        future: "Take a chance! Keep this hope alive. Whatever the current crisis, you will figure out a solution for it. Ask for a raise or try something new, luck is on your side.",
+        image: "images/star.png"
     },
     {
         name: "The Moon",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "Your current struggle is likely wrapped up in a recent period of confusion. You may have used your creativity to make order from the chaos to help you cope. You made the best of your past as you could, look back on those days as having been fruitful rather than negative.",
+        present: "You are getting so many mixed signals from life right now. Things that have comforted you in the past are no longer there to sooth you. Temptations threaten to take over, and you are falling out of reality. This is a chance to better yourself by not believing in the false assumptions of societal norms. Create your own way, live a life of your own design.",
+        future: "You may soon find yourself wandering from your current path and what is important to you to pursue a hobby or path that is entirely different. If you are in a creative field, afflictions like boredom, or staleness will soon be forgotten as you are hit with a rush of inspiration.",
+        image: "images/moon.png"
     },
     {
         name: "The Sun",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "Your foundation is in optimism. Your energy and charisma help you make the most of all situations. You had a fundamentally wonderful childhood, the only negativity is missing those beautiful days gone by. Focus on what you have now. You are likely on a great path though you may walk it alone.",
+        present: "You have everything to gain. You have the ability to get exactly what you want and be the center of attention while doing so. You have little to lose and little consequence if you reach out and take what you want. Operate in a transparent manner so that the integrity of your success can not be questioned.",
+        future: "You will soon see yourself coming out ahead. This isn't just a short term gain, you are on a path leading to happiness and even enlightenment.",
+        image: "images/sun.png"
     },
     {
         name: "Judgment",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "You've made a big decision in the recent or distant past to adhere to one way of thinking. This wasn't necessarily the right way of thinking. Regardless your current path is based on this decision.",
+        present: "Your biggest internal conflict right now is concerning a large decision you must make. You understand that this choice is final but others may not view it as seriously as you do. This decision, in fact, is a large one that you will have to make of your own free will.",
+        future: "The misery and burdens you have been carrying with you are about to be relieved. A coming event will finally allow you to see a world of possibility. You'll look back on yourself as a shell when you begin this new lease on life with a renewed sense of purpose.",
+        image: "images/judgment.png"
     },
     {
         name: "The World",
-        past: ,
-        present: ,
-        future: ,
-        image: 
+        past: "The foundation of your current situation is built on a huge accomplishment from your past. Know that this moment of fullfilment has certainly passed and you would be wise to move forward on this foundation and not dwell in the past.",
+        present: "You are at a powerfully affirming climax in your life. The culmination of hard work and dreams about to be achieved. Though you may be getting what you have always wanted don't be surprised if you are facing a lot of anxiety because this kind of situation is very rare in life. Be careful not to sabotage your recent gains due to anxiety, enjoy the fruits of your labor.",
+        future: "You must keep your dreams alive. You are probably questioning your ability to put the necessary energy into achieving your dream, this card is telling you to move forward with vigor and confidence. You have a lot to offer and a great ability to achieve.",
+        image: "images/world.png"
     },
 ]
