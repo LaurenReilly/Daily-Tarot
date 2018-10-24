40b63a2dc92645915e9c41266bad8bdf6954696a
Lauren Reilly <LaurenReillyNet@gmail.com>
2018-10-24 14:41:10 -0400

    added past present and future readings up until the lovers

--- data.js
+++ data.js
@@ -1,156 +1,156 @@
 var majorArcana = [
     {
         name: "The Fool",
-        past: ,
-        present: ,
-        future: ,
-        image: ,
+        past: "Impulsivity and rash actions have led to time wasting activities. You may have been invovled with an unconventional person who has led you astray.",
+        present: "Your search to find who you truly are is causing you to abandon commitments and constraints. Your sense of free-will and openness allows you to truly sieze the day.",
+        future: "You will enjoy the fresh beauty of a new life off of the beaten path. An exciting person who you view as very unique may soon captivate you.",
+        image: 
     },
     {
         name: "The Magician",
-        past: ,
-        present: ,
-        future: ,
-        image: ,
+        past: "You've displayed your natural talents and you are enjoying success based on your accomplishments.",
+        present: "You have a chance to use your unique skills to impress important individuals. Luck and skill are in your favor, sieze this opportunity!",
+        future: "You've taken the time to build the skills needed to earn everything you desire. When opportunity arises you can follow the necessary path to achieve your dream.",
+        image: 
     },
     {
         name: "The High Priestess",
-        past: ,
-        present: ,
-        future: ,
-        image: ,
+        past: "You've needed to have great patience and understanding. You have an ability to get by on your gut feelings and the knowledge you've aquired is a steady foundation for the future.",
+        present: "Avoid the current conflict. What is being presented to you is not likely true. You may feel that your current situation can be improved, you are correct.",
+        future: "That hunch you have about a coming conflict is correct. Any attempts to get you involved in consipiring against someone should be avoided at all costs.",
+        image: 
     },
     {
         name: "The Empress",
-        past: ,
-        present: ,
-        future: ,
-        image: ,
+        past: "Have you been acting like a diva lately? Immersed in material possesions? This is likely why you're having difficulty in current relationships.",
+        present: "Things are great right now, so great that you might be feeling a little bored. Maybe its time to delve deeper into a hobby or relationship for greater satisfaction?",
+        future: "Good things are coming your way, like fairytale ending kind of good. Wealth, love, and happiness are in store.",
+        image: 
     },
     {
         name: "The Emperor",
-        past: ,
-        present: ,
-        future: ,
-        image: ,
+        past: "This card represents an authority figure in your life who set a firm boundary of what is acceptable for you. If you are in a good place you have this person to thank, however if this person abused their authority they are to blame for your current feelings of loneliness and alienation.",
+        present: "Be confident in following a straight and narrow path to your current goal. An outside influencer may have helped you to see this path more clearly, move forward boldly.",
+        future: "Your present decisions no matter how small will have a large impact on the near future. A change will soon storm into your life demanding that you follow a specific behavior and will require that you either submit or resist.",
+        image: 
     },
     {
         name: "The Heirophant",
-        past: ,
-        present: ,
-        future: ,
-        image: ,
+        past: "Your past was defined largely by a structured culture or system of belief. There is a strong sense of identity tied up in your past.",
+        present: "You have begun to take interest in a new belief system whether social, political, or even a new daily routine. Your engagement and adherence offers a satisfying feeling of belonging and accomplishment.",
+        future: "A person will enter your life who will explain the meaning of life to you as well as your role in it. Depending on the person, this could be a great experience or very detrimental to you. Be open but wary.",
+        image: 
     },
     {
         name: "The Lovers",
-        past: ,
-        present: ,
-        future: ,
-        image: ,
+        past: "The foundation of your current situation is based largely in a loving relationship. If you are in a long term union this is a confirmation that your relationship defines your identity. If you are single, this could point to a past love or even the relationship between your parents.",
+        present: "You are bubbling with passion over a budding romance. If you are single now, this card signifies a sudden end to that status. The power of this relationship will be transformative over all aspects of your life.",
+        future: "Approach the future of your dating life with a clear idea of what you want and need. The intense affection of new relationships can blind you to reality, so have a clear picture of your ideals before this person enters your life.",
+        image: 
     },
     {
         name: "The Chariot",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
     {
         name: "Strength",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
     {
         name: "The Hermit",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
     {
         name: "Wheel of Fortune",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
     {
         name: "Justice",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
     {
         name: "The Hanged Man",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
     {
         name: "Death",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
     {
         name: "Temperance",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
     {
         name: "The Devil",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
     {
         name: "The Tower",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
     {
         name: "The Star",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
     {
         name: "The Moon",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
     {
         name: "The Sun",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
     {
         name: "Judgment",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
     {
         name: "The World",
         past: ,
         present: ,
         future: ,
-        image: ,
+        image: 
     },
 ]
